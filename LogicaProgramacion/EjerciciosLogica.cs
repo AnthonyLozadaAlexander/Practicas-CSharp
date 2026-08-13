c96d237d21e90f7405801e418783739cfe532408
zDefcon21 <antholozadarequelme@gmail.com>
2026-08-12 19:56:43 -0500

    Agrega clase EjerciciosLogica con método para verificar año bisiesto
    Se implementa la clase EjerciciosLogica que incluye un método para determinar si un año es bisiesto. El método solicita al usuario un año y valida la entrada, retornando un valor booleano que indica si el año es bisiesto o no.

---
+++ LogicaProgramacion/EjerciciosLogica.cs
@@ -0,0 +1,32 @@
+﻿namespace LogicaProgramacion;
+
+public class EjerciciosLogica
+{
+    public bool EjercicioAnioBisisesto()
+    {
+        bool result = false;
+        int anio = 0;
+        
+        Console.WriteLine("Ingrese El Año: ");
+        anio = Int32.Parse(Console.ReadLine());
+        if (anio.Equals(null) || anio <= 0)
+        {
+            Console.WriteLine("El anio ingresado no es valido");
+            
+        }else if (anio % 4 == 0)
+        {
+            result = true;
+            if (anio % 100 == 0)
+            {
+                result = false;
+                if (anio % 400 == 0)
+                {
+                    result = true;
+                }
+            }
+        }
+        
+        return result;
+    }
+}
+
