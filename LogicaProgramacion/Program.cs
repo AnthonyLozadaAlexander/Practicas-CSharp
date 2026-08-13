7830ca223af82e5f4059edfe1105aa846aa5ea0e
zDefcon21 <antholozadarequelme@gmail.com>
2026-08-12 19:57:02 -0500

    Agrega archivos de configuración y proyectos a la solución
    Se añaden archivos .gitignore, encodings.xml, indexLayout.xml, y archivos de proyecto para LogicaProgramacion y Practica_Recursividad. También se incluye un archivo Program.cs con una estructura básica para la ejecución del programa.

---
+++ LogicaProgramacion/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LogicaProgramacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool result = false;
+            EjerciciosLogica Logica = new EjerciciosLogica();
+            /*result = Logica.EjercicioAnioBisisesto();
+            Console.WriteLine("El anio ingresado es Bisiesto: " + (result ? "Si" : "No"));*/
+            
+        }
+    }
+}
