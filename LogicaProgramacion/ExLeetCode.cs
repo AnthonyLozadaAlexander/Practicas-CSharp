be7e9b4fea5fbf02d43240187b9c995474d8fe18
zDefcon21 <antholozadarequelme@gmail.com>
2026-08-15 03:19:07 -0500

    Agrega clase ExLeetCode con método para encontrar número faltante
    Se implementa la clase ExLeetCode que incluye el método MissingNumber. Este método calcula el número faltante en un arreglo de enteros que contiene números del 0 al n. La implementación utiliza la fórmula de la suma de los primeros n números enteros para determinar el número faltante de manera eficiente.

---
+++ LogicaProgramacion/ExLeetCode.cs
@@ -0,0 +1,21 @@
+﻿namespace LogicaProgramacion;
+
+public class ExLeetCode
+{
+    public int MissingNumber(int[] nums)
+    {
+        int n = 0, sumReal = 0, sumExpected = 0, missingNumber = 0;
+        n = nums.Length;
+        
+        foreach(int r in nums)
+        {
+            sumReal = sumReal + r;
+        }
+
+        sumExpected = ((n * (n + 1)) / 2);
+
+        missingNumber = sumExpected - sumReal;
+        
+        return missingNumber;
+    }
+}
