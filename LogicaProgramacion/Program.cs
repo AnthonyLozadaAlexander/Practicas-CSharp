a47ea0c67b1daf02728dbf3c0cc75c97474ef43b
zDefcon21 <antholozadarequelme@gmail.com>
2026-08-15 03:18:59 -0500

    Agrega menú de selección para ejercicios de lógica y LeetCode
    Se implementa un menú en la función Main que permite al usuario seleccionar entre ejercicios de lógica y ejercicios de LeetCode.
    Se agrega la opción para ejecutar el método EjercicioFibonacci y se incluye un ejemplo de uso del método MissingNumber.

--- LogicaProgramacion/Program.cs
+++ LogicaProgramacion/Program.cs
@@ -7,11 +7,33 @@ namespace LogicaProgramacion
         static void Main(string[] args)
         {
             bool result = false;
-            EjerciciosLogica Logica = new EjerciciosLogica();
-            /*result = Logica.EjercicioAnioBisisesto();
-            Console.WriteLine("El anio ingresado es Bisiesto: " + (result ? "Si" : "No"));*/
+            ExLogica Logica = new ExLogica();
+            ExLeetCode LeetCode = new ExLeetCode(); 
             
-            Logica.EjercicioFibonacci();
+            Console.WriteLine("-----------Menu-----------");
+            Console.WriteLine("1. Ejercicios Logica");
+            Console.WriteLine("2. Ejercicios LeetCode");
+            Console.WriteLine("--------------------------");
+            Console.Write("Seleccione Una Opcion: "); String opcion = Console.ReadLine();
+
+            switch (opcion)
+            {
+                case "1":
+                    /*result = Logica.EjercicioAnioBisisesto();
+            Console.WriteLine("El anio ingresado es Bisiesto: " + (result ? "Si" : "No"));*/
+                    Logica.EjercicioFibonacci();
+                    
+                    break;
+                case "2":
+                    int r = LeetCode.MissingNumber(new int[] { 3, 0, 1 });
+                    Console.WriteLine("Result: " + r);
+                    break;
+                default:
+                    Console.WriteLine("Opcion Invalida");
+                    break;
+            }
+            
+            
         }
     }
 }
