6eb0523478ed4100381b30f65ff70352b05d04b3
zDefcon21 <antholozadarequelme@gmail.com>
2026-08-25 05:09:04 -0500

    Agrega archivos de proyecto y clase principal
    Se crea el archivo de proyecto 'PrincipiosPOO.csproj' con la configuración básica para una aplicación .NET.
    Se añade la clase 'Program' en 'Program.cs' que inicializa un objeto 'Human' con propiedades básicas.

---
+++ PrincipiosPOO/Main/Program.cs
@@ -0,0 +1,17 @@
+﻿using PrincipiosPOO.Clases;
+
+namespace PrincipiosPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Human francisco = new Human();
+            francisco.Name = "Francisco";
+            francisco.Height = 1.69;
+            francisco.weight = 82;
+            francisco.Age = 22;
+        }
+    }
+}
