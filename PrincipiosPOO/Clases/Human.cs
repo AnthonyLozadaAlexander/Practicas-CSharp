f30e16d5a95de58594a18c22104fd5a0d4754510
zDefcon21 <antholozadarequelme@gmail.com>
2026-08-25 05:00:36 -0500

    Agrega clase Human con propiedades básicas

---
+++ PrincipiosPOO/Clases/Human.cs
@@ -0,0 +1,9 @@
+﻿namespace PrincipiosPOO.Clases;
+
+public class Human
+{
+    public string Name { get; set; }
+    public int Height { get; set; }
+    public int  weight { get; set; }
+    public int Age { get; set; }
+}
