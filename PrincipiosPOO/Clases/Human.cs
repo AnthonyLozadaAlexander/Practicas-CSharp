1410969e115aac15bb15aea0a924dbdf0d2e72ec
zDefcon21 <antholozadarequelme@gmail.com>
2026-08-25 05:08:59 -0500

    Cambia tipo de propiedad Height en clase Human a double

--- PrincipiosPOO/Clases/Human.cs
+++ PrincipiosPOO/Clases/Human.cs
@@ -3,7 +3,7 @@
 public class Human
 {
     public string Name { get; set; }
-    public int Height { get; set; }
+    public double Height { get; set; }
     public int  weight { get; set; }
     public int Age { get; set; }
 }
