ebfa8d434b82534dc51e0faa7e6917e9c7816cec
zDefcon21 <antholozadarequelme@gmail.com>
2026-08-25 08:14:19 -0500

    Corrige el comentario en la propiedad Owner y mejora la legibilidad del código en el constructor de BankAccount

--- PrincipiosPOO/Constructores/BankAccount.cs
+++ PrincipiosPOO/Constructores/BankAccount.cs
@@ -4,7 +4,7 @@ namespace PrincipiosPOO.Constructores
     {
         public int accountNumber { get; set; } // numero de cuenta
 
-        public string Owner => _owner;
+        public string Owner => _owner; // el atributo Owner toma la referencia de _owner, que es privado y solo puede ser modificado por el constructor
 
         private string _owner;
 
@@ -14,6 +14,8 @@ namespace PrincipiosPOO.Constructores
 
         public BankAccount(string name)
         {
+            var random = new Random();
+            accountNumber = random.Next(); // genera un numero de cuenta aleatorio
             _owner = name;
         }
 
