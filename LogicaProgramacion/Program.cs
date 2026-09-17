1c1429cf0badd4f83e222d9237146d101874a9b9
zDefcon21 <antholozadarequelme@gmail.com>
2026-09-17 03:32:38 -0500

    Agrega opción para el ejercicio "Two Sum" en el menú de LeetCode y permite ingresar un arreglo y un objetivo.

--- LogicaProgramacion/Program.cs
+++ LogicaProgramacion/Program.cs
@@ -46,6 +46,7 @@ namespace LogicaProgramacion
                 case "2":
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.WriteLine("\n1. Ejercicio Missing Number");
+                    Console.WriteLine($"2. Ejercicio Two Sum");
                     Console.Write("Seleccione Una Opcion -> ");
                     string op = Console.ReadLine();
                     switch (op)
@@ -53,6 +54,30 @@ namespace LogicaProgramacion
                         case "1":
                             int r = LeetCode.MissingNumber(new int[] { 3, 0, 1 });
                             Console.WriteLine("Result: " + r);
+                            break;
+                        case "2":
+                            int[] arr;
+                            Console.WriteLine("Ingrese El Tamanio Del Arreglo");
+                            Console.Write(" -> "); int size = Convert.ToInt32(Console.ReadLine());
+                            arr = new int[size];
+
+                            Console.WriteLine("Ingrese Los Elementos Del Arreglo");
+                            for (int i = 0; i < arr.Length; i++)
+                            {
+                                Console.Write($"[{i}]: "); 
+                                arr[i] = Convert.ToInt32(Console.ReadLine());
+                            }
+
+                            Console.WriteLine("Ingrese El Target");
+                            Console.Write(" -> "); int target = Convert.ToInt32(Console.ReadLine());
+
+                            int[] result = LeetCode.TwoSum(arr, target);
+
+                            for (int i = 0; i < result.Length; i++)
+                            {
+                                Console.WriteLine($"Index = {result[i]}");
+                            }
+
                             break;
                         default:
                             Console.WriteLine("Opcion Invalida");
